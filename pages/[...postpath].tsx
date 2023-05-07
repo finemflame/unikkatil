33b2b0eb9e5077dd61518c549eb6905caa02548b
finemflame <84194780+finemflame@users.noreply.github.com>
2023-05-07 14:28:18 +0200

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -11,74 +11,86 @@ interface Cache {
 const cache: Cache = {};
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const endpoint = process.env.GRAPHQL_ENDPOINT as string;
-  const graphQLClient = new GraphQLClient(endpoint);
-  const referringURL = ctx.req.headers?.referer || null;
-  const pathArr = ctx.query.postpath as Array<string>;
-  const path = pathArr.join('/');
-  console.log(path);
-  const fbclid = ctx.query.fbclid;
+  try {
+    const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+    const graphQLClient = new GraphQLClient(endpoint);
+    const referringURL = ctx.req.headers?.referer || null;
+    const pathArr = ctx.query.postpath as Array<string>;
+    const path = pathArr.join('/');
+    console.log(path);
+    const fbclid = ctx.query.fbclid;
 
-  // Check if the query result is already in the cache
-  if (cache[path]) {
-    return {
-      props: cache[path],
-    };
-  }
+    // Check if the query result is already in the cache
+    if (cache[path]) {
+      console.log('Using cached data for', path);
+      return {
+        props: cache[path],
+      };
+    }
 
-  // Redirect if facebook is the referer or request contains fbclid
-  if (referringURL?.includes('facebook.com') || fbclid) {
-    return {
-      redirect: {
-        permanent: false,
-        destination: `${endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)}`,
-      },
-    };
-  }
+    // Redirect if facebook is the referer or request contains fbclid
+    if (referringURL?.includes('facebook.com') || fbclid) {
+      console.log('Redirecting from', referringURL);
+      const destination = `${endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)}`;
+      console.log('Redirecting to', destination);
+      return {
+        redirect: {
+          permanent: false,
+          destination,
+        },
+      };
+    }
 
-  const query = gql`
-    {
-      post(id: "/${path}/", idType: URI) {
-        id
-        excerpt
-        title
-        link
-        dateGmt
-        modifiedGmt
-        content
-        author {
-          node {
-            name
+    const query = gql`
+      {
+        post(id: "/${path}/", idType: URI) {
+          id
+          excerpt
+          title
+          link
+          dateGmt
+          modifiedGmt
+          content
+          author {
+            node {
+              name
+            }
           }
-        }
-        featuredImage {
-          node {
-            sourceUrl
-            altText
+          featuredImage {
+            node {
+              sourceUrl
+              altText
+            }
           }
         }
       }
-    }
-  `;
+    `;
 
-  const data = await graphQLClient.request(query);
-  if (!data.post) {
+    const data = await graphQLClient.request(query);
+    if (!data.post) {
+      return {
+        notFound: true,
+      };
+    }
+
+    // Store the query result in the cache for future requests
+    const result = {
+      path,
+      post: data.post,
+      host: ctx.req.headers.host,
+    };
+    cache[path] = result;
+    console.log('Storing data in cache for', path);
+
+    return {
+      props: result,
+    };
+  } catch (error) {
+    console.error('Error fetching data:', error);
     return {
       notFound: true,
     };
   }
-
-  // Store the query result in the cache for future requests
-  const result = {
-    path,
-    post: data.post,
-    host: ctx.req.headers.host,
-  };
-  cache[path] = result;
-
-  return {
-    props: result,
-  };
 };
 
 interface PostProps {
@@ -110,8 +122,11 @@ const Post: React.FC<PostProps> = (props) => {
         <meta property="article:published_time" content={post.dateGmt} />
         <meta property="article:modified_time" content={post.modifiedGmt} />
         <meta property="og:image" content={post.featuredImage.node.sourceUrl} />
-        <meta property="og:image:alt" content={post.featuredImage.node.altText || post.title} />
-    <title>{post.title}</title>
+        <meta
+          property="og:image:alt"
+          content={post.featuredImage.node.altText || post.title}
+        />
+        <title>{post.title}</title>
       </Head>
       <div className="post-container">
         <h1>{post.title}</h1>
